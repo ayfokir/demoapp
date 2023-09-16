50f3b587a4102e02e5665f2692b1f5f642bc294c
ayfokir <ayfotsega19.27@gmail.com>
2023-09-16 11:36:00 +0300

    add pages to the frontend

--- frontend/demoapp/src/App.js
+++ frontend/demoapp/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Home from './Pages/Home';
+import AddEmployee from './Pages/AddEmployee';
+import Login from './Pages/Login';
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
